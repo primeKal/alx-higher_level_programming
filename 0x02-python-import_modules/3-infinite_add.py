371f3aa735f1cb0ab934ac3df71941d40b34f47a
kaleb <kalebteshale72@gmail.com>
2023-09-10 23:03:14 -0700

    updates

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,8 +3,8 @@
 if __name__ == "__main__":
     """Print the addition of all arguments."""
     import sys
-
+    num_args = len(sys.argv)
     total = 0
-    for i in range(len(sys.argv) - 1):
-        total += int(sys.argv[i + 1])
-    print("{}".format(total))
+    for i in range(1, num_args):
+        total += int(sys.argv[i])
+    print("{:d}".format(total))
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     import hidden_4
 
     names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
-            print(name)
+    for i in range(0, len(names)):
+        if names[i][:2] != "__":
+            print(names[i])
