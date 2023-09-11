1b92a38d7c3ec93600fc843070d942762f36f709
Kaleb Teshale <kalebteshale72@gmail.com>
2023-09-11 13:28:11 +0300

    hiiilo

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def print_reversed_list_integer(my_list = []):
+def print_reversed_list_integer(my_list=[]):
     if not my_list:
         return
     for i in reversed(my_list):
